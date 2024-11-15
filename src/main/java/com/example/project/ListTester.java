9be94a95c9d4a96e1a585aea444991ec39213412
ibra1 <ibra1@ibra>
2024-11-15 05:12:03 +0300

    change 5

--- src/main/java/com/example/project/ListTester.java
+++ src/main/java/com/example/project/ListTester.java
@@ -3,40 +3,25 @@ package com.example.project;
 public class ListTester<T> {
     public static <T> void circularLeftShift(List <T> list, int n)
     {    
-    	DLL<T> tmp=new DLL();
-	list.findFirst();
-	for(int i=0; i<n;i++) {
-		
-	tmp.insert(list.retrieve());
-	list.remove();
-	
-	if(list.empty())
-		break;
-	
-	}
-	
-	
-	if(!list.empty())
-	while(!list.last()) {
-		list.findNext();
-	}
-
-	while(!tmp.last()) {
-		tmp.findNext();
-	}
-	
-while(!tmp.first()) {
-	list.insert(tmp.retrieve());
-	tmp.findPrevious();
-	
-}
-list.insert(tmp.retrieve());
-    }
+    	for( int i = 0; i < n ; i ++) {
+    		list.findFirst () ;
+    		
+    		T elem = list . retrieve () ;
+    		list.remove () ;
+    		
+    		while (! list.last ())
+    		list.findNext () ;
+    		list . insert ( elem ) ;
+    		
+    	}
+    		}
+    
     
     
     
     public static <T> void reverseCopy(DLL<T> l1, DLL<T> l2) {
-
+    	if(l1.empty())
+    	return;
     	while(!l1.last()) {
     		l1.findNext();
     	
